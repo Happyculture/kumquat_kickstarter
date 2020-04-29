5d734b8f716460c868307c6e8fde910392f4745e
Edouard Cunibil <edouard@happyculture.coop>
2020-04-29 17:51:18 +0200

    Add migration for basic field storages.

---
+++ src/Plugin/migrate/process/FieldTypeFromName.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\hc_site_builder\Plugin\migrate\process;
+
+use Drupal\Core\Field\FieldTypePluginManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\MigrateSkipRowException;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "field_type_from_name"
+ * )
+ */
+class FieldTypeFromName extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The field type plugin manager.
+   *
+   * @var \Drupal\Core\Field\FieldTypePluginManagerInterface
+   */
+  protected $fieldTypePluginManager;
+
+  /**
+   * The migration object.
+   *
+   * @var \Drupal\migrate\Plugin\MigrationInterface
+   */
+  protected $migration;
+
+  /**
+   * Constructs a FieldType plugin.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Field\FieldTypePluginManagerInterface $fieldTypePluginManager
+   *   The field type plugin manager.
+   * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+   *   The migration being run.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FieldTypePluginManagerInterface $fieldTypePluginManager, MigrationInterface $migration = NULL) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->fieldTypePluginManager = $fieldTypePluginManager;
+    $this->migration = $migration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.field.field_type'),
+      $migration
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $field_type_name = is_array($value) ? $value[0] : $value;
+
+    foreach ($this->fieldTypePluginManager->getDefinitions() as $definition) {
+      if ($definition['label'] == $field_type_name) {
+        return $definition['id'];
+      }
+    }
+
+    throw new MigrateSkipRowException(sprintf("No field type plugin found for '%s'.", $field_type_name));
+  }
+
+}
