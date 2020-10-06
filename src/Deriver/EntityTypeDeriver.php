80094d4a9ecf90af609e6898286d759686376470
Edouard Cunibil <edouard@happyculture.coop>
2020-10-06 18:33:44 +0200

    Fix bundle label migration for entities not using 'name' as key.
    Change-Id: I0d313d15d8fb8cb9b8a265f34ab7716dd66efa24

--- src/Deriver/EntityTypeDeriver.php
+++ src/Deriver/EntityTypeDeriver.php
@@ -77,6 +77,9 @@ class EntityTypeDeriver extends DeriverBase implements ContainerDeriverInterface
     $base_plugin_definition['process'][$entityType->getKey('id')] = $base_plugin_definition['process']['_machine_name'];
     unset($base_plugin_definition['process']['_machine_name']);
 
+    $base_plugin_definition['process'][$entityType->getKey('label')] = $base_plugin_definition['process']['_label'];
+    unset($base_plugin_definition['process']['_label']);
+
     $base_plugin_definition['destination']['plugin'] = 'entity:' . $entityType->id();
 
     return $base_plugin_definition;
